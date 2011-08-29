0fb6ea16fbf6708435f6e6c8135b1b4c1a84b071
doula <doula@yorick.qa.smonk.lan>
2011-08-29 16:54:06 -0700

    remove comments

--- doulabot/bot.py
+++ doulabot/bot.py
@@ -215,17 +215,6 @@ class BaseBot(SimpleIRCClient):
             self.connection.disconnect()
 
 
-# parser = argparse.ArgumentParser(description='Process some integers.')
-#  parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                    help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                    const=sum, default=max,
-#                    help='sum the integers (default: find the max)')
-
-# args = parser.parse_args()
-# print args.accumulate(args.integers)
-
-
 def run_doulabot(args=None):
     if args is None:
         parser = argparse.ArgumentParser(description='Run the doulabot')
